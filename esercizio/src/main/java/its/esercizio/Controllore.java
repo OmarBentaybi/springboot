172f19ee2ec8d58a00dea1bc629643fc291acdc8
OmarBentaybi <o.bentaybi@itsrizzoli.it>
2022-06-13 14:59:24 +0200

    Initial commit

---
+++ esercizio/src/main/java/its/esercizio/Controllore.java
@@ -0,0 +1,19 @@
+package its.esercizio;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+    @Controller
+    public class Controllore {
+        @GetMapping("/somma")
+        public String Somma(@RequestParam("valore") int val1, @RequestParam("valore2") int val2, Model model) {
+            int somma = val1 + val2;
+            model.addAttribute("risultato", somma);
+            return "result";
+        }
+
+    }
+
---
+++ esercizio/src/test/java/its/esercizio/EsercizioApplicationTests.java
@@ -0,0 +1,13 @@
+package its.esercizio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsercizioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
